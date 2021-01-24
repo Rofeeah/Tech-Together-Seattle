632942cf52af61750726376ae836a9a8e1c54894
Rofeeah Ayeni <ayenirofeeah@gmail.com>
2021-01-24 19:25:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// //Bot POPUP
+
+// var bot = document.getElementById("poptext");
+
+// // bot.onclick = function() {
+// //    modal.style.display = "popuptext";
+// //  }
+
+//  function openForm() {
+//   document.getElementById("botHelp").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("botHelp").style.display = "none";
+// }
+
+// function chatResponse(){
+
+// num = Math.ceil(Math.random()*3);
+
+// var greets = ["hello", "hi", "hey"]
+
+// for (var i=0; i< greets.length; i++){
+//   if (greets[i] >= 1){
+//     alert(greets[num]);
+//   }
+// }
+
+
+// }
+// chatResponse();
+
+//  console.log(num);
